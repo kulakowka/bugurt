14ab72413d2f91aaa9d138cd2ace2aa8c8d34ea4
kulakowka <kulakowka@gmail.com>
2016-01-20 22:24:56 +0300

    add russian pluralization

--- config/viewHelpers.js
+++ config/viewHelpers.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const moment = require('moment')
+const pluralize = require('pluralize-ru')
 
 module.exports = function viewHelpers (req, res, next) {
   moment.locale('ru')
@@ -8,5 +9,6 @@ module.exports = function viewHelpers (req, res, next) {
   res.locals.env = process.env.NODE_ENV
   res.locals.moment = moment
   res.locals.currentUser = req.user
+  res.locals.pluralize = pluralize
   next()
 }

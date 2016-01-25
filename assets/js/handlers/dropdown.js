4e4273b31d55c2f3d9a8c12054a032682ee983df
kulakowka <kulakowka@gmail.com>
2016-01-25 19:53:04 +0300

    black & white design

--- assets/js/handlers/dropdown.js
+++ assets/js/handlers/dropdown.js
@@ -6,6 +6,7 @@ module.exports.onHandleClick = function onHandleClick (event) {
   var handle = $(this)
   var dropdown = handle.closest('.dropdown')
 
+  $('.dropdown').removeClass('open')
   dropdown.addClass('open')
 
   $(document).one('click', function () {
